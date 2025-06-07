1d5c05ed1fe08290bedf17fcbdd5468a8b2ceb6e
Isaiah Ozadhe <tekkieware@gmail.com>
2025-06-07 12:55:47 +0100

    removed websocket service

--- websocket/abstractions/notifier.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Notifier(ABC):
-    @abstractmethod
-    async def notify(self, message: str):
-        pass
--- websocket/app.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from services.websocket_service import notify_clients
-
-if __name__ == "__main__":
-    asyncio.run(notify_clients())
