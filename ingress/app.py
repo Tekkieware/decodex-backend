d597139bb4856fe4512136cf60541d18a5d265d4
Isaiah Ozadhe <tekkieware@gmail.com>
2025-07-07 09:29:28 +0100

    small fixes

--- ingress/app.py
+++ ingress/app.py
@@ -10,10 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 # Shared WebSocket adapter
