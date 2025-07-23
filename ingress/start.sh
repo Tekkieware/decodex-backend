23bc48b2c700808b87f7192d52bdb7bc91cec5b8
Isaiah Ozadhe <tekkieware@gmail.com>
2025-07-23 10:38:43 +0100

    added init and ingress

--- ingress/start.sh
+++ ingress/start.sh
@@ -1,8 +1,10 @@
-
+#!/bin/bash
 set -e
 
+export PYTHONPATH=/app  # Make sure all folders (services, adapters, analysis) are importable
+
 echo "Starting Analysis Worker..."
-python -m analysis.worker &
+python analysis/app.py &
 
 echo "Starting FastAPI Server..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app:app --host 0.0.0.0 --port 8000
